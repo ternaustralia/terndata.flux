a29ccfcbf50a81d9632324e419829a1e2f1665d2
Yong Liaw - 1683019 <y.liaw@uq.edu.au>
2025-06-11 13:48:03 +1000

    Add comment on numpy binary incompatibility warning can be ignored.

--- ci-scripts/run-tests.sh
+++ ci-scripts/run-tests.sh
@@ -15,6 +15,12 @@ pip install -r requirements-test.txt
 #   junit_logging=no|log|system-out|system-err|out-err|all
 #   jnuti_duration_report=total|call
 #   junit_family=legacy|xunit1|xunit2
+#
+# Note: We can ignore runtime warning "numpy.ndarray size changed" as 
+# numpy actually ignores them. https://github.com/numpy/numpy/blob/main/numpy/__init__.py#L320-L323.
+# This is a known issue with netCDF4. See https://github.com/unidata/netcdf4-python/issues/1354.
+# The issue can be reproduced by:
+# python -c "import numpy;import warnings;warnings.filterwarnings('error');import netCDF4"
 pytest  -W ignore::DeprecationWarning -W ignore::RuntimeWarning --junit-xml=./test-reports/xunit.xml "$@"
 
 # do we want coverage reporting?
